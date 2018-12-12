b3bc481a1ff891893c8b0d0512f71e4203b6bbde
admin <>
2018-12-12 20:50:50 +0800

    no message

---
+++ ASPNetRazorPageDemo/Pages/QueryCpty.cshtml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace ASPNetRazorPageDemo.Pages
+{
+    public class QueryCptyModel : PageModel
+    {
+        public string Message { get; set; }
+        public List<string> MessageList { get; set; }
+
+        public void OnGet()
+        {
+            Message = "系統時間："+DateTime.Now.ToString() ;
+        }
+
+        public JsonResult OnGetList()
+        {
+            List<string> lstString = new List<string>
+            {
+                "Val 1",
+                "Val 2",
+                "Val 3"
+            };
+            return new JsonResult(lstString);
+        }
+
+        public ActionResult OnPostSend()
+        {
+            string sPostValue1 = "";
+            string sPostValue2 = "";
+            string sPostValue3 = "";
+            {
+                MemoryStream stream = new MemoryStream();
+                Request.Body.CopyTo(stream);
+                stream.Position = 0;
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string requestBody = reader.ReadToEnd();
+                    if(requestBody.Length > 0)
+                    {
+                        var obj = JsonConvert.DeserializeObject<PostData>(requestBody);
+                        if(obj != null)
+                        {
+                            sPostValue1 = obj.Item1;
+                            sPostValue2 = obj.Item2;
+                            sPostValue3 = obj.Item3;
+                        }
+                    }
+                }
+            }
+            List<string> lstString = new List<string>
+            {
+                sPostValue1,
+                sPostValue2,
+                sPostValue3
+            };
+            return new JsonResult(lstString);
+        }
+    }
+
+    
+}
